aa032a62c9f57430a3417ffe27e4ac329e5f1908
Viktar <vitykolosov@gmail.com>
2024-03-02 13:28:17 +0100

    test base

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './app/theme/theme.ts';
 import { AuthProvider } from './app/hooks/useAuth.tsx';
-import App from './App.tsx';
+
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/routes/router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AuthProvider>
-        <App />
+        <RouterProvider router={router} />
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
