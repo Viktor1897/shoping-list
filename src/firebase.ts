ee62a9fa8729e86647c0c9f304563b416b2ce2a9
Viktar <vitykolosov@gmail.com>
2024-03-30 15:51:21 +0100

    move sensetive data to the .env file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID,
+} = import.meta.env;
+
 //TODO move senseitive data to .env file
 const firebaseConfig = {
-  apiKey: 'AIzaSyD88YYwO-D4sPNwXqVSDpKuoUVcq7is4HM',
-  authDomain: 'household-ef131.firebaseapp.com',
-  projectId: 'household-ef131',
-  storageBucket: 'household-ef131.appspot.com',
-  messagingSenderId: '229413205973',
-  appId: '1:229413205973:web:dcd82c411539b6a59baf5c',
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
